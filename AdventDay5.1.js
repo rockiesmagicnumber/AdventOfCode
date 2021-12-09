b0b776d7596daf7648f2a3d93eaf64509bcbde51
Andrew Martin <rockiesmagicnumber@gmail.com>
2021-12-08 20:50:05 -0700

    I should really just scrap today's work. It does seem close, though...

--- AdventDay5.1.js
+++ AdventDay5.1.js
@@ -1,23 +1,41 @@
 const coords = require("./AdventDay5.data.min.js");
 
 function reviewPaths() {
+    let squares = [];
     for (let coord of coords) {
-        let squares = getCoveredSquares(coord);
-        //let countedSquares = squares.reduce()	
+        let cs = getCoveredSquares(...coord);
+        !!cs[0] && squares.push(...cs[0]);
     }
+    let countedSquares = aggregateSquares(squares);
+    let multipleSquares = countedSquares.filter(x => countedSquares[x] > 1);
+    console.log(multipleSquares.length);
+}
+
+function aggregateSquares(squares) {
+    REWORK THIS
+    // let grouping = [];//squares.map(x => { return { "x": x[0], "y": x[1] }; });
+    // for (square of squares) {
+    //     let name = square[0] + "-" + square[1];
+    //     if (!grouping[name]) {
+    //         grouping.push({ name, 1});
+    //     } else {
+    //         grouping[name]++;
+    //     }
+    // }
+    // return grouping;
 }
 
 function getCoveredSquares(x1, y1, x2, y2) {
     let squares = [];
-    squares.push(getHorizontalLines(x1, x2, y1));
-    squares.push(getVerticalLines(x1, y1, y2));
+    y1 === y2 && squares.push(getHorizontalLines(x1, x2, y1));
+    x1 === x2 && squares.push(getVerticalLines(x1, y1, y2));
     return squares;
 }
 
 function getHorizontalLines(x1, x2, y) {
     let squares = [];
     for (let i = Math.min(x1, x2); i <= Math.max(x1, x2); i++) {
-        squares.push({ x: i, y: y });
+        squares.push([i, y]);
     }
     return squares;
 }
@@ -25,9 +43,9 @@ function getHorizontalLines(x1, x2, y) {
 function getVerticalLines(x, y1, y2) {
     let squares = [];
     for (let i = Math.min(y1, y2); i <= Math.max(y1, y2); i++) {
-        squares.push({ x: x, y: i });
+        squares.push([x, i]);
     }
     return squares;
 }
 
-getCoveredSquares(coords[0]);
+reviewPaths();
