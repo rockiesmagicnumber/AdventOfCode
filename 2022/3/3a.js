f03b47dc640e1f470909677dc5884483a9aa2ec8
Andrew Martin <rockiesmagicnumber@gmail.com>
2022-12-06 22:56:57 -0700

    boom day3 part 1 first shot

--- 2022/3/3a.js
+++ 2022/3/3a.js
@@ -1,6 +1,30 @@
-fs = require('fs');
+fs = require("fs");
 fs.readFile("input.txt", "utf8", (err, inputData) => {
     if (!!err) {
         console.error(err);
     }
+
+    let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let totalScore = 0;
+
+    inputData.split("\n").forEach(row => {
+        let midpoint = row.length / 2;
+        let str1 = row.substring(0, midpoint);
+        let str2 = row.substring(midpoint);
+        console.log(str1, str2);
+        let common = "";
+
+        for (var i = 0; i < str1.length; i++) {
+            if (str2.indexOf(str1[i]) > -1) {
+                common = str1[i];
+                break;
+            }
+        }
+
+        let score = alphabet.indexOf(common) + 1;
+        console.log(common, score);
+        totalScore += score;
+    });
+
+    console.log(totalScore);
 });
