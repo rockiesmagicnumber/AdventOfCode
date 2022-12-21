86468daf11d2c89938b17b46a852fbf262595063
rockiesmagicnumber <rockiesmagicnumber@gmail.com>
2022-12-20 23:03:12 -0700

    2022/day6 (#17)
    * initial
    
    * commit
    
    * day 6 done

---
+++ 2022/6/6a.js
@@ -0,0 +1,19 @@
+fs = require('fs');
+fs.readFile("input.txt", "utf8", (err, inputData) => {
+    if (!!err) {
+        console.error(err);
+    }
+
+    let data = inputData;
+    console.log(data);
+
+    for (let i = 4; i < data.length; i++) {
+        let bloc = data.slice(i - 4, i);
+        let blocDistinct = Array.from(new Set(bloc));
+        console.log(blocDistinct);
+        if (bloc.length === blocDistinct.length) {
+            console.log("Set found after character number " + i, bloc);
+            break;
+        }
+    }
+});
